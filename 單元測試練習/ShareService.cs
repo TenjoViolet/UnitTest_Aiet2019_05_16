7ec2e1430b71f9d29e9b47202680760f8c4113fb
TenjoViolet@gmail.com <TenjoViolet@gmail.com>
2019-05-16 10:47:55 +0800

    加入專案檔案。

---
+++ 單元測試練習/ShareService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace UnitTestLab1
+{
+    public class ShareService
+    {
+        public string ValidateMember(string account, string password)
+        {
+            DataBaseRepository dbr = new DataBaseRepository();
+            DataTable dtMember = new DataTable();
+            ArrayList aryFild = new ArrayList();
+            ArrayList aryValue = new ArrayList();
+            string strSql = "";
+            strSql = @" select * from Member where account=@account  ";
+            aryFild.Add("account");
+            aryValue.Add(account);
+
+            dtMember = dbr.SelectDataTable(dbr.GetConnString(), strSql, aryFild, aryValue);
+
+            if (dtMember.Rows.Count == 0)
+            {
+                return "No Account!";
+            }
+            else if (dtMember.Rows[0]["password"].ToString() != password)
+            {
+                return "Password Error!";
+            }
+            else
+            {
+                return "Success!";
+            }
+
+        }
+
+
+    }
+}

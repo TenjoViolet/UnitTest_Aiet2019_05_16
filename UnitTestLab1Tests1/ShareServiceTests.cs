a7e1d2e0ab3f29470e94c20c00017ceac91fe402
TenjoViolet@gmail.com <TenjoViolet@gmail.com>
2019-05-16 15:00:39 +0800

    試寫第一個單元測試傳入帳號密碼_若無此帳號_回傳NoAccount

---
+++ UnitTestLab1Tests1/ShareServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestLab1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestLab1.Tests
+{
+    [TestClass()]
+    public class ShareServiceTests
+    {
+        [TestMethod()]
+        public void ValidateMemberNoAccountTest_傳入帳號密碼_若無此帳號_回傳NoAccount()
+        {
+            ShareService shareService = new ShareService();
+            string account = "";
+            string password = "";
+            string expected = "";
+            string actual = "";
+            account = "adminErr";
+            password = "admin123";
+            expected = "No Account!";
+            actual = shareService.ValidateMemberNoAccount(account, password);
+
+            Assert.AreEqual(expected, actual);
+
+            // Assert.Fail();
+
+        }
+    }
+}
